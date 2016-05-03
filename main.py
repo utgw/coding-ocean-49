500bcfb91fbd6f7d2392c29a92354fc5006c2875
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-03 13:39:47 +0900

    とりあえず一通り

---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from sqlalchemy.orm import sessionmaker
+from PIL import Image
+import models
+app = Flask(__name__)
+app.debug = True
+Session = sessionmaker(bind=models.engine)
+image_dir = 'image/'
+
+
+@app.route('/')
+def index():
+    session = Session()
+    images = session.query(models.Image)
+    return render_template('index.html', images=images)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    session = Session()
+    new_id = session.query(models.Image).count() + 1
+    image_path = '{}{}.png'.format(image_dir, new_id)
+    f = request.files['file']
+    image = models.Image(id=new_id, created_at=datetime.now())
+    f.save(image_path)
+    img = Image.open(image_path)
+    thumbnail_path = '{}thumbnail/{}.png'.format(image_dir, new_id)
+    img.thumbnail((128, 128))
+    img.save(thumbnail_path)
+    session.add(image)
+    session.commit()
+    return redirect(url_for('show_image_page', id=new_id))
+
+
+@app.route('/{}<id>.png'.format(image_dir))
+def show_image(id):
+    mode = request.args.get('mode', '')
+    if mode == 'thumbnail':
+        image_path = '{}thumbnail/{}.png'.format(image_dir, id)
+    else:
+        image_path = '{}{}.png'.format(image_dir, id)
+    return send_file(image_path)
+
+
+@app.route('/{}<id>'.format(image_dir))
+def show_image_page(id):
+    session = Session()
+    images = session.query(models.Image).filter(models.Image.id == id).one()
+    return render_template('show_image.html', id=id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
+
---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+Base = declarative_base()
+engine = create_engine('sqlite:///pyazo.db', echo=False)
+
+class Image(Base):
+    __tablename__ = 'images'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
